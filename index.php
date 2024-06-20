ad3931a37c9f1a0bc388ec65f0485d19492a7c52
sven borremans <Sven>
2024-06-20 10:29:29 +0200

    doxc(leerdoel) alle mapjes voor de leerdoelen toegevoegd

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Portfolio Kevin</title>
+<style>
+       body {
+           display: flex;
+           flex-direction: column;
+           align-items: center;
+       }
+</style>
+</head>
+<body>
+<h1>De portfolio van Kevin</h1>
+<h3>Van welk leerdoel wil jij de opdrachten zien</h3>
+<p><a href="../SLOB/leerdoel1/leerdoel1.php">Weet goed wat deze opleiding inhoudt en of deze opleiding bij je past</a></p>
+<p><a href="../SLOB/leerdoel2/leerdoel2.php">Je kunt je persoonlijke kwaliteiten benoemen en uitleggen</a></p>
+<p><a href="../SLOB/leerdoel3/leerdoel3.php">Je ontwikkelt studievaardigheden en kunt deze inzetten</a></p>
+<p><a href="../SLOB/leerdoel4/leerdoel4.php">Je kunt reflecteren volgens verschillende manieren en deze inzetten</a></p>
+<p><a href="../SLOB/leerdoel5/leerdoel5.php"> Je krijgt meer inzicht in jouw motieven om in de ICT te werken</a></p>
+<p><a href="../SLOB/leerdoel6/leerdoel6.php">Je ontwikkelt je persoonlijk maar ook beroepsmatig</a></p>
+<p><a href="../SLOB/leerdoel7/leerdoel7.php">Je weet wat en goede beroepshouding is, en wat het beroep inhoudt</a></p>
+<p><a href="../SLOB/leerdoel8/leerdoel8.php">Je weet in welke branches je kunt werken als je gediplomeerd bent en wat het werk inhoudt</a></p>
+<p><a href="../SLOB/leerdoel9/leerdoel9.php">Je maakt een beargumenteerde keuze over het vervolg na je opleiding</a></p>
+<p><a href="../SLOB/leerdoel10/leerdoel10.php">Je leert verantwoordelijkheid te nemen voor je eigen leerproces</a></p>
+</html>
